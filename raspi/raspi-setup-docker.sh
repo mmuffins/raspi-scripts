a74edd46d2944086ce6ce104a182f98e954e2e65
Unknown <mail@tiv73.com>
2019-08-17 23:46:42 +0200

    improve formatting

--- raspi/raspi-setup-docker.sh
+++ raspi/raspi-setup-docker.sh
@@ -14,7 +14,7 @@ configdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ ! -f $configdir/raspi-setup-functions.sh ];
 then
-	echo -e "\e[91mraspi-setup-functions not found in $configdir/raspi-setup-functions, exiting script"; tput sgr0
+	echo -e "\e[91mraspi-setup-functions not found in $configdir/raspi-setup-functions, exiting script";tput sgr0
 	exit 2
 fi
 
@@ -23,31 +23,31 @@ source "$configdir/raspi-setup-functions.sh"
 echo ""
 echo ""
 echo -e "${MAGENTA}Docker setup${NORMAL}"
-echo -e "${LBLUE}Checking needed config files...${NORMAL}"
+echo -e "${BLUE}Checking needed config files...${NORMAL}"
 
-echo -e "${LBLUE}Installing dependencies...${NORMAL}"
+echo -e "${BLUE}Installing dependencies...${NORMAL}"
 sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
 
-echo -e "${LBLUE}Installing docker...${NORMAL}"
+echo -e "${BLUE}Installing docker...${NORMAL}"
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 
-echo -e "${LBLUE}Setting user permissions...${NORMAL}"
+echo -e "${BLUE}Setting user permissions...${NORMAL}"
 sudo groupadd docker
 sudo usermod -aG docker michael
 sudo usermod -aG docker mmuffins
 
-echo -e "${LBLUE}Importing Docker CPG key...${NORMAL}"
+echo -e "${BLUE}Importing Docker CPG key...${NORMAL}"
 sudo curl https://download.docker.com/linux/raspbian/gpg
 
-echo -e "${LBLUE}Setting up the docker repo...${NORMAL}"
+echo -e "${BLUE}Setting up the docker repo...${NORMAL}"
 echo "deb https://download.docker.com/linux/raspbian/ buster stable" | sudo tee -a /etc/apt/sources.list > /dev/null
 
-echo -e "${LBLUE}Updating...${NORMAL}"
+echo -e "${BLUE}Updating...${NORMAL}"
 sudo apt-get update && sudo apt-get upgrade
 
-echo -e "${LBLUE}Starting docker...${NORMAL}"
+echo -e "${BLUE}Starting docker...${NORMAL}"
 sudo systemctl enable docker
 sudo systemctl start docker.service
 docker info
 
-echo -e "${LBLUE}Try running 'docker run hello-world' to see if everything is set up correctly${NORMAL}"
+echo -e "${BLUE}Try running 'docker run hello-world' to see if everything is set up correctly${NORMAL}"
